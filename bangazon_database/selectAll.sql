90dfd71a809a3e25ea03483a00e20b7679f733e9
MrErin <erinmeaker@gmail.com>
2018-07-26 09:28:21 -0500

    working

---
+++ bangazon_database/selectAll.sql
@@ -0,0 +1,16 @@
+-- Use this to verify that there is data in the database
+
+SELECT * FROM bangazonapi_employee LIMIT 5;
+SELECT * FROM bangazonapi_employee_training LIMIT 5;
+SELECT * FROM bangazonapi_computer LIMIT 5;
+SELECT * FROM bangazonapi_cust_order LIMIT 5;
+SELECT * FROM bangazonapi_department LIMIT 5;
+SELECT * FROM bangazonapi_employment_Dates LIMIT 5;
+SELECT * FROM bangazonapi_ordered_products LIMIT 5;
+SELECT * FROM bangazonapi_payment_options LIMIT 5;
+SELECT * FROM bangazonapi_payment_type LIMIT 5;
+SELECT * FROM bangazonapi_product LIMIT 5;
+SELECT * FROM bangazonapi_product_type LIMIT 5;
+SELECT * FROM bangazonapi_training_program LIMIT 5;
+SELECT * FROM bangazonapi_training_program_sessions LIMIT 5;
+SELECT * FROM bangazonapi_user LIMIT 5;
--- bangazonapi/management/commands/faker_factory.py
+++ bangazonapi/management/commands/faker_factory.py
@@ -3,41 +3,25 @@ from django_seed import Seed
 seeder = Seed.seeder()
 import random
 from bangazonapi.models import
-Training_Program,
-Training_Program_Sessions,
-User,
-Employees,
-Employment_Dates,
-Department,
-Computer,
-Employee_Training,
-Product,
-Product_Type,
-Cust_Order,
-Ordered_Products,
-Payment_Options,
-Payment_Type
 
 
 class Command(BaseCommand):
 
-  def handle(self, *args, **options):
-    seeder.add_entity(Training_Program, 12) # the number argument is the total num of rows you want created
-    seeder.add_entity(Training_Program_Sessions, 12)
-    seeder.add_entity(User, 12)
-    seeder.add_entity(Employees, 12)
-    seeder.add_entity(Employment_Dates, 12)
-    seeder.add_entity(Department, 12)
-    seeder.add_entity(Computer, 12)
-    seeder.add_entity(Employee_Training, 12)
-    seeder.add_entity(Product, 25)
-    seeder.add_entity(Product_Type, 12)
-    seeder.add_entity(Cust_Order, 12)
-    seeder.add_entity(Ordered_Products, 12)
-    seeder.add_entity(Payment_Options, 12)
-    seeder.add_entity(Payment_Type, 12)
-    # seeder.add_entity(Payment_Type, 50, {'acct_number': lambda x: random.randint(11111,99999)})
-    # Override the seeder "guess" of what faker provider you want it to use
-    # seeder.add_entity(Product, 100, {'name': lambda x: seeder.faker.catch_phrase()})
+    def handle(self, *args, **options):
+        # the number argument is the total num of rows you want created
+        seeder.add_entity(Training_Program, 12)
+        seeder.add_entity(Training_Program_Sessions, 12)
+        seeder.add_entity(User, 12)
+        seeder.add_entity(Employees, 12)
+        seeder.add_entity(Employment_Dates, 12)
+        seeder.add_entity(Department, 12)
+        seeder.add_entity(Computer, 12)
+        seeder.add_entity(Employee_Training, 12)
+        seeder.add_entity(Product, 25)
+        seeder.add_entity(Product_Type, 12)
+        seeder.add_entity(Cust_Order, 12)
+        seeder.add_entity(Ordered_Products, 12)
+        seeder.add_entity(Payment_Options, 12)
+        seeder.add_entity(Payment_Type, 12)
 
-    inserted_pks = seeder.execute()
+        inserted_pks = seeder.execute()
