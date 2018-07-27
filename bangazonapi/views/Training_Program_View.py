8d0a67674f2b36141b4c8a7afded82a607b42e1e
pwmurphy89 <pwmurphy89@gmail.com>
2018-07-27 13:50:00 -0500

    specified verbs (get,post, put, etc) for EmployeeView, TrainingProgramView, DepartmentView

--- bangazonapi/views/Training_Program_View.py
+++ bangazonapi/views/Training_Program_View.py
@@ -1,6 +1,11 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status, mixins
 from bangazonapi.serializers import Training_ProgramSerializer
 from bangazonapi.models import Training_Program
+
+from rest_framework.response import Response
+from django.http import Http404
+import datetime
+
 
 class Training_Program_View(viewsets.ModelViewSet):
     """
@@ -8,3 +13,17 @@ class Training_Program_View(viewsets.ModelViewSet):
     """
     queryset = Training_Program.objects.all()
     serializer_class = Training_ProgramSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            instance_time = instance.start_date
+            now = datetime.datetime.now().date()
+            if now < instance_time:
+                self.perform_destroy(instance)
+            else:
+                Response(content, status=status.HTTP_403_FORBIDDEN)
+
+        except Http404:
+            pass
+        return Response(status=status.HTTP_204_NO_CONTENT)
