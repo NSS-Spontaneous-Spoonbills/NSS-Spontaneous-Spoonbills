88c15ecf3f03eadb06bfee8f3f122ed3046f6464
MrErin <erinmeaker@gmail.com>
2018-07-27 13:55:39 -0500

    fixing merge conflicts

--- bangazonapi/urls.py
+++ bangazonapi/urls.py
@@ -2,10 +2,10 @@
 This file is responsible for handling all the routing for each of our pages through Rest Framework : DefaultRouter
 """
 from django.urls import path
-from views import Computer_View
-from views import Payment_Type_View
-from views import Product_Type_View
-from views import Training_Program_Sessions_View
+from .views import Computer_View
+from .views import Payment_Type_View
+from .views import Product_Type_View
+from .views import Training_Program_Sessions_View
 from .views import User_View
 from .views import Product_View
 from .views import Employee_Training_View
@@ -22,15 +22,21 @@ router = DefaultRouter()
 router.register('computer', Computer_View, base_name='computer')
 router.register('product_type', Product_Type_View, base_name='product_type')
 router.register('payment_type', Payment_Type_View, base_name='payment_type')
-router.register('training_program_sessions', Training_Program_Sessions_View, base_name='training_program_sessions')
+router.register('training_program_sessions',
+                Training_Program_Sessions_View, base_name='training_program_sessions')
 router.register('user', User_View, base_name='user')
 router.register('product', Product_View, base_name='product')
-router.register('employee_training', Employee_Training_View, base_name='employee_training')
+router.register('employee_training', Employee_Training_View,
+                base_name='employee_training')
 router.register('cust_order', Cust_Order_View, base_name='cust_order')
-router.register('employment_dates', Employment_Dates_View,base_name='employment_dates')
-router.register('ordered_products', Ordered_Products_View, base_name='ordered_products')
-router.register('payment_options', Payment_Options_View, base_name='payment_options')
-router.register('employee', Employee_View,base_name='employee')
+router.register('employment_dates', Employment_Dates_View,
+                base_name='employment_dates')
+router.register('ordered_products', Ordered_Products_View,
+                base_name='ordered_products')
+router.register('payment_options', Payment_Options_View,
+                base_name='payment_options')
+router.register('employee', Employee_View, base_name='employee')
 router.register('department', Department_View, base_name='department')
-router.register('training_program', Training_Program_View, base_name='training_program')
+router.register('training_program', Training_Program_View,
+                base_name='training_program')
 urlpatterns = router.urls
--- bangazonapi/views/__init__.py
+++ bangazonapi/views/__init__.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import User
+from rest_framework import viewsets
 
 from .User_View import User_View
 from .Cust_Order_View import Cust_Order_View
@@ -13,5 +15,3 @@ from .Training_Program_Sessions_View import Training_Program_Sessions_View
 from .Payment_Type_View import Payment_Type_View
 from .Product_Type_View import Product_Type_View
 from .Computer_View import Computer_View
-from django.contrib.auth.models import User
-from rest_framework import viewsets
