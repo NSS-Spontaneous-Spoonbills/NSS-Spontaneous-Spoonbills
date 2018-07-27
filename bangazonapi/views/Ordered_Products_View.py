0b2726506dbaa557379f83b78660ee3ce8591716
JacobAndrewSmith92 <jacobandrewsmith92@gmail.com>
2018-07-27 11:16:39 -0500

    Final: Changed viewset to view

--- bangazonapi/views/Ordered_Products_View.py
+++ bangazonapi/views/Ordered_Products_View.py
@@ -3,7 +3,7 @@ from bangazonapi.serializers import Ordered_ProductsSerializer
 from bangazonapi.models import Ordered_Products
 
 
-class Ordered_ProductsViewSet(viewsets.ModelViewSet):
+class Ordered_Products_View(viewsets.ModelViewSet):
     """
     API endpoint that allows Ordered_Products to be viewed or edited.
     """
