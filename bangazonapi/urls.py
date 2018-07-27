01d02b00c0462a2c4e88512482853456a86edf30
JacobAndrewSmith92 <jacobandrewsmith92@gmail.com>
2018-07-27 11:11:49 -0500

    Changed viewset to view for filenames

--- bangazonapi/urls.py
+++ bangazonapi/urls.py
@@ -5,10 +5,10 @@ from django.urls import path
 from .views import User_View
 from .views import Product_View
 from .views import Employee_Training_View
-from .views import Cust_OrderViewSet
-from .views import Employment_DatesViewSet
-from .views import Ordered_ProductsViewSet
-from .views import Payment_OptionsViewSet
+from .views import Cust_Order_View
+from .views import Employment_Dates_View
+from .views import Ordered_Products_View
+from .views import Payment_Options_View
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -16,8 +16,8 @@ router = DefaultRouter()
 router.register('user', User_View, base_name='user')
 router.register('product', Product_View, base_name='product')
 router.register('employee_training', Employee_Training_View, base_name='employee_training')
-router.register('cust_order', Cust_OrderViewSet, base_name='cust_order')
-router.register('employment_dates', Employment_DatesViewSet,base_name='employment_dates')
-router.register('ordered_products', Ordered_ProductsViewSet, base_name='ordered_products')
-router.register('payment_options', Payment_OptionsViewSet, base_name='payment_options')
+router.register('cust_order', Cust_Order_View, base_name='cust_order')
+router.register('employment_dates', Employment_Dates_View,base_name='employment_dates')
+router.register('ordered_products', Ordered_Products_View, base_name='ordered_products')
+router.register('payment_options', Payment_Options_View, base_name='payment_options')
 urlpatterns = router.urls
--- bangazonapi/views/__init__.py
+++ bangazonapi/views/__init__.py
@@ -1,8 +1,8 @@
 from .User_View import User_View
-from .Cust_Order_View import Cust_OrderViewSet
-from .Employment_Dates_View import Employment_DatesViewSet
-from .Ordered_Products_View import Ordered_ProductsViewSet
-from .Payment_Options_View import Payment_OptionsViewSet
+from .Cust_Order_View import Cust_Order_View
+from .Employment_Dates_View import Employment_Dates_View
+from .Ordered_Products_View import Ordered_Products_View
+from .Payment_Options_View import Payment_Options_View
 from .Product_View import Product_View
 from .Employee_Training_View import Employee_Training_View
 
