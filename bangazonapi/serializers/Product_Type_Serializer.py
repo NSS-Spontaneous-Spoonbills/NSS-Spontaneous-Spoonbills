3da5c0db17e022a22a7c5857955740e34f6ed5fb
David Paul <dfp333@gmail.com>
2018-07-30 10:18:27 -0500

    added serializer docstrings for computer, product/payment type, training program sessions

--- bangazonapi/serializers/Product_Type_Serializer.py
+++ bangazonapi/serializers/Product_Type_Serializer.py
@@ -3,5 +3,6 @@ from bangazonapi.models import Product_Type
 
 class Product_TypeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
+        """product description serializer """
         fields = '__all__'
         model = Product_Type
